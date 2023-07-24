2c37a99db6de7a30c332ee8c66a82ad012ef988f
jsebasrojas29 <jsebasrojas22@gmail.com>
2023-07-24 18:48:17 +0200

    Proto 1.0

---
+++ MyWhisky-vue/src/data/guitarras.js
@@ -0,0 +1,72 @@
+export const db = [
+    {
+        id: 1,
+        nombre: 'Whisky Black And White 200ml',
+        imagen: 'whisky_01',
+        descripcion: 'Descubre este destilado único, elaborado a partir de malta y con un agradable Grado de alcohol del 40%. Disfrútalo con responsabilidad. ¡Salud!',
+        precio: 20000,
+    },
+    {
+        id: 2,
+        nombre: 'Whisky Black And White 8 Años',
+        imagen: 'whisky_02',
+        descripcion: 'Whisky joven de la casa Buchanans con una excelente relación calidad y precio. El exceso de alcohol es perjudicial para la salud. Ley 30 de 1986. Prohíbase el expendio de bebidas embriagantes a menores de edad ley 124 1994.',
+        precio: 50000,
+    },
+    {
+        id: 3,
+        nombre: 'Whisky Black & White X 375ml ',
+        imagen: 'whisky_03',
+        descripcion: 'Es un whisky fresco con una presencia aromática de hierbas y una suavidad delicada. Su mezcla excepcionalmente suave y equilibrada, utiliza los mejores whiskies de malta de Speyside y los escoceses de Highland.',
+        precio: 54000,
+    },
+    {
+        id: 4,
+        nombre: 'Jack Daniel`s Honey',
+        imagen: 'whisky_04',
+        descripcion: 'Disfruta de un exquisito sabor con el Jack Daniels Honey, una perfecta combinación de la icónica marca Jack Daniels y la dulzura de la miel. Este bourbon de la línea Honey te ofrece una experiencia única y deliciosa. ',
+        precio: 120000,
+    },
+    {
+        id: 5,
+        nombre: 'Jack Daniels Fire',
+        imagen: 'whisky_05',
+        descripcion: 'La Bodega del licor. Jack Daniel’s Fire es un whisky americano de Tennessee. Está compuesto por una base de Jack Daniel’s Old Nº7 infusionado con licor de canela roja. ',
+        precio: 119000,
+    },
+    {
+        id: 6,
+        nombre: 'Jack Daniels Apple',
+        imagen: 'whisky_06',
+        descripcion: 'Somos La Bodega Del Licor, Jack Daniel’s Tennessee Apple se elabora a partir de la expresión central de la marca, que ha sido suavizada al carbón y madurada en barricas nuevas de roble americano, y licor con sabor a manzana.',
+        precio: 129000,
+    },
+    {
+        id: 7,
+        nombre: 'Whiskey Jack Daniels Miniatura',
+        imagen: 'whisky_07',
+        descripcion: 'Elegante miniatura de whisky Jack Daniels con el tamaño perfecto para ser usado como gesto de cortesía en un evento especial o como regalo.',
+        precio: 17000,
+    },
+    {
+        id: 8,
+        nombre: 'Whisky Old Parr 12 Años',
+        imagen: 'whisky_08',
+        descripcion: 'La historia del whisky escocés Old Parr 12 Años comienza a principios del siglo XIX. Este whisky, elegante y sofisticado, combina maltas con diferentes perfiles de sabor, ofreciendo una síntesis superior de todo lo que Escocia tiene para ofrecer en términos de este popular licor ámbar.',
+        precio: 130000,
+    },
+    {
+        id: 9,
+        nombre: 'Grants Litro 1000ml',
+        imagen: 'whisky_09',
+        descripcion: 'Grant’s es un whisky que se madura en barriles de cerveza, y hasta el día de hoy es el único whisky del mundo con esta característica. Tiene un valor afrutado, amelado, muy exquisito para el paladar',
+        precio: 105000,
+    },
+    {
+        id: 10,
+        nombre: 'Baileys',
+        imagen: 'whisky_10',
+        descripcion: 'Baileys es una crema de whisky irlandés. Hecho a base de ingredientes como vainilla, chocolate, azúcar y cacao. Juntos crean un delicioso liquido de consistencia cremosa. ',
+        precio: 105000,
+    }
+]
